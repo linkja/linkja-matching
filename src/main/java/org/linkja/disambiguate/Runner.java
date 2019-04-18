835d4e5fdd4e419a4c28609aec37326e526dfac4
Luke Rasmussen <luke.rasmussen@gmail.com>
2019-04-18 09:23:01 -0500

    Set up GUI and CLI modes

--- src/main/java/org/linkja/disambiguate/Runner.java
+++ src/main/java/org/linkja/disambiguate/Runner.java
@@ -1,7 +1,21 @@
 package org.linkja.disambiguate;
 
+import java.util.Arrays;
+
 public class Runner {
   public static void main(String[] args) {
-    GlobalMatchTasks.main(args);
+    if (args.length == 0) {
+      GlobalMatchSqlite.main(args);
+    }
+    else {
+      // Check to see if the user specified that they want to run with a GUI or not
+      String guiOption = Arrays.stream(args).filter(x -> x.equalsIgnoreCase("--gui")).findFirst().orElse(null);
+      if (guiOption != null) {
+        GlobalMatchTasks.main(args);
+      }
+      else {
+        GlobalMatchSqlite.main(args);
+      }
+    }
   }
 }
